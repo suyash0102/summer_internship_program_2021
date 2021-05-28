f49b9070c5c31fffc8e85c09fc1183bb389a12ca
suyash0102 <59247598+suyash0102@users.noreply.github.com>
2021-05-28 09:38:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# summer_internship_program_2021
+ 
---
+++ task_01/prediction.py
@@ -0,0 +1,8 @@
+import joblib
+name = input('Kindly enter your name: ')
+numyexp = float(input('Kindly enter your experience in no. of years: '))
+model=joblib.load('salary_prediction.pk1')
+print(f'\nName: {name}')
+print(f'Years of experience you have: {numyexp}')
+output=model.predict([[numyexp]])
+print(f'{name}, your  monthly predicted salary in INR is: {round(output[0],2)}')                
